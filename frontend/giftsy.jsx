11687d8ca6e28dff19a925fc28c9a27502d82906
Anshuj <anshuj@gmail.com>
2019-08-01 10:40:45 -0700

    removed bootstrapped user from window

--- frontend/giftsy.jsx
+++ frontend/giftsy.jsx
@@ -6,13 +6,6 @@ import Root from "./components/root";
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   let preloadedState = undefined;
-  if (window.currentUser) {
-    preloadedState = {
-      session: {
-        currentUser: window.currentUser
-      }
-    };
-  }
   const store = createStore(preloadedState);
   ReactDOM.render(<Root store={store} />, root);
 });
